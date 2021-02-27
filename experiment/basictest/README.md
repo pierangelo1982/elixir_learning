9b981c542d1148f9d1866fecf8f92b8da1886509
pierangelo1982 <pierangelo1982@gmail.com>
2021-02-27 12:40:44 +0100

    get my odds numbers

---
+++ experiment/basictest/README.md
@@ -0,0 +1,21 @@
+# EvenOdd
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `basictest` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:basictest, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/basictest](https://hexdocs.pm/basictest).
+
---
+++ experiment/basictest/lib/even_odd.ex
@@ -0,0 +1,75 @@
+defmodule EvenOdd do
+
+  require Integer
+
+  @moduledoc """
+  Documentation for `EvenOdd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EvenOdd.create_number_list()
+      :[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  """
+
+  def create_number_list do
+    [1,2,3,4,5,6,7,8,9]
+  end
+
+  @doc """
+  is_odd_or_even
+  ## Examples
+
+      iex(2)> EvenOdd.is_odd_or_even(43)
+        "43 è Dispari"
+        iex(3)> EvenOdd.is_odd_or_even(44)
+        "44 è Pari"
+
+  """
+  def is_odd_or_even(number) do
+    case Integer.is_odd(number) do 
+      :true -> "#{number} è Dispari" 
+      :false -> "#{number} è Pari" 
+    end
+  end
+
+
+ @doc """
+  list_number
+  ## Examples
+
+      iex(9)> number = EvenOdd.create_number_list
+      [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+      iex(10)> EvenOdd.list_number(number)        
+      ["1 è Dispari", "2 è Pari", "3 è Dispari", "4 è Pari", "5 è Dispari",
+      "6 è Pari", "7 è Dispari", "8 è Pari", "9 è Dispari"]
+
+  """
+
+  def list_number(numbers) do 
+    for value <- numbers do
+      is_odd_or_even(value)
+    end
+  end
+
+ @doc """
+  get_my_numbers
+  ## Examples
+
+      EvenOdd.get_my_numbers             
+      ["1 è Dispari", "2 è Pari", "3 è Dispari", "4 è Pari", "5 è Dispari",
+      "6 è Pari", "7 è Dispari", "8 è Pari", "9 è Dispari"]
+
+  """
+
+  def get_my_numbers do
+    EvenOdd.create_number_list
+    |> EvenOdd.list_number
+  end
+
+end
